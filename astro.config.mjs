1eb54ed9faefee71bdf668d5786a81a234009bcc
Oriol Ferràndez Grau <oriol@ferrandez.cat>
2025-08-22 09:40:52 +0200

    First repo with minimal structure using i18n and dinamic url for blog

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+
+export default {
+    integrations: [
+        react(),
+        tailwind()
+    ],
+    output: 'static',
+    build: {
+        static: true
+    }
+}
